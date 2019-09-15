914f3f1916b5b3d6f0694c873b92c1da216872a3
TheodorTE18D <54800526+TheodorTE18D@users.noreply.github.com>
2019-09-15 19:15:08 +0200

    Update HemläxaV37.java

--- src/HemläxaV37.java
+++ src/HemläxaV37.java
@@ -2,13 +2,16 @@
  * 
  */
 import java.util.Scanner; 
+
 /**
  * @author theodor.niklasson
  *
  */
 public class HemläxaV37 {
-	void main(){
+	public static void main(String[] args){
 		Scanner input = new Scanner(System.in);
+		System.out.print("Vad heter du?");
+		String user = input.nextLine();
     }
     
 }
